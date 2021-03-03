7bf1e23c1fd8e1eee54e9a588542f305f6f07f24
levakuz <levakuz007@gmail.com>
2021-03-03 14:34:48 +0300

    added id

--- sender.py
+++ sender.py
@@ -1,6 +1,6 @@
 from requests import post
 
-url = 'http://195.2.74.79:8001/carident/by/2021-03'
+url = 'http://195.2.74.79:8001/carident/by/2021-03/816269'
 # files = {'file': open('img.jpg', 'rb')}
 # foo = post('http://0.0.0.0:8001/caident/by/2021.01', files=files)
 files = {'file': open('1440.jpg', 'rb')}
